f559f0246d49676aa450bf626e81293263f01db4
Rhymmor <anatolybelonog@gmail.com>
2019-02-13 02:21:05 +0700

    add tinkoff api manager

---
+++ src/server/modules/tinkoff-api-manager.ts
@@ -0,0 +1,29 @@
+import {TinkoffApi, AccessLevel} from 'tinkoff-api';
+import { isOk } from '../lib/utils';
+import { ICredentials } from '../../lib/types/api';
+
+export class TinkoffApiManager {
+    private api = new TinkoffApi();
+
+    public async getSession(): Promise<string> {
+        const res = await this.api.initializeSession();
+        return res.resultCode;
+    }
+
+    public async singUp(credentials: ICredentials, session?: string): Promise<string> {
+        let currentSession = session;
+        if (!isOk(currentSession)) {
+            currentSession = await this.getSession();
+        }
+        const signUp = await this.api.signUp(currentSession, credentials);
+        return signUp.operationTicket;
+    }
+
+    public async confirmSignUp(session: string, ticketId: string, smsId: string): Promise<void> {
+        await this.api.confirmSignUp(session, ticketId, smsId);
+        const {payload: {accessLevel}} = await this.api.levelUp(session);
+        if (accessLevel !== AccessLevel.CLIENT) {
+            throw new Error(`Couldn't confirm session (${session}) - access level is ${accessLevel}`);
+        }
+    }
+}
