0f3cc4421311d986f9af6659606743e6529bd83a
Rhymmor <anatolybelonog@gmail.com>
2019-03-30 19:00:11 +0700

    add scss

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,5 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import { AppRouter } from "./app-router";
+
+import "semantic-ui-css/semantic.min.css";
+import "./index.scss";
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
