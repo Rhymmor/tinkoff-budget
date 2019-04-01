9c3c57ce820c528f13dda8a0e27c51a1c16c970d
Rhymmor <anatolybelonog@gmail.com>
2019-04-01 21:21:52 +0700

    add page loader

--- src/client/components/layout/SuspenseWrapper.tsx
+++ src/client/components/layout/SuspenseWrapper.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import { Loader } from "semantic-ui-react";
 
 interface IProps {
     children: React.ReactNode;
 }
 
 export function SuspenseWrapper(props: IProps) {
-    return <React.Suspense fallback={<div>Loading... </div>}>{props.children}</React.Suspense>;
+    return <React.Suspense fallback={<Loader active size="massive" />}>{props.children}</React.Suspense>;
 }
