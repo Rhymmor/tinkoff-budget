d73826fdafaf3a398f4e22cb5b0026d1c12cfd73
Rhymmor <anatolybelonog@gmail.com>
2019-04-01 22:34:18 +0700

    add request middleware

--- src/client/lib/api/request.ts
+++ src/client/lib/api/request.ts
@@ -1,9 +1,21 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+// tslint:disable-next-line:no-var-requires
+const { Service } = require("axios-middleware");
 
 const request = axios;
 
+const service = new Service(axios);
+
 export { request };
 
-export function setRequestMiddleware(modify: (cfg: AxiosRequestConfig) => Promise<AxiosResponse>) {
-    request.defaults.adapter = modify;
+export function setRequestMiddleware(modify?: (cfg: AxiosRequestConfig) => AxiosRequestConfig) {
+    if (modify) {
+        service.register({
+            onRequest(config: AxiosRequestConfig) {
+                return modify(config);
+            }
+        });
+    } else {
+        service.reset();
+    }
 }
