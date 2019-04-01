e436d0cee7e4247d573cde31c1bcba48522403d9
Rhymmor <anatolybelonog@gmail.com>
2019-04-01 22:36:22 +0700

    middleware for session id

--- src/client/redux/models/session.ts
+++ src/client/redux/models/session.ts
@@ -5,6 +5,8 @@ import { mutate } from "../../lib/utils";
 import { ApiSessionUtils } from "../../lib/api/login";
 import { IStore } from ".";
 import { logger } from "../../lib/logger";
+import { setRequestMiddleware } from "../../lib/api/request";
+import { IApiCommonQuery } from "../../../lib/types/rest/tinkoff/common";
 
 export interface ISessionStore {
     session: ISessionIdStore;
@@ -35,22 +37,31 @@ export const sessionModel = {
         }
     },
     effects: (dispatch: any) => ({
-        initSession: (_force = false, _rootState?: IStore) => {
+        initSession: (_force = false) => {
             dispatch.session.modifySessionId((store: ISessionIdStore) => {
                 store.state = AsyncStoreState.Loading;
             });
             return ApiSessionUtils.getSession()
                 .then(res => {
+                    const { session } = res.data;
+                    const sessionQuery: IApiCommonQuery = { session };
+                    setRequestMiddleware(cfg => {
+                        cfg.params = { ...cfg.params, ...sessionQuery };
+                        return cfg;
+                    });
                     return dispatch.session.modifySessionId((store: ISessionIdStore) => {
-                        store.sessionId = res.data.session;
+                        store.sessionId = session;
                         store.state = AsyncStoreState.Success;
+                        store.error = undefined;
                     });
                 })
                 .catch(e => {
                     logger.error(e);
+                    setRequestMiddleware(undefined);
                     return dispatch.session.modifySessionId((store: ISessionIdStore) => {
-                        store.error = String(e);
+                        store.sessionId = undefined;
                         store.state = AsyncStoreState.Failed;
+                        store.error = String(e);
                     });
                 });
         }
