77d2c4d3f32108ef5994866016ff85b6e5d9d1b8
Rhymmor <anatolybelonog@gmail.com>
2019-11-10 03:27:05 +0700

    support async/await

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,8 +1,10 @@
+import "regenerator-runtime/runtime";
+
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { render } from "react-dom";
 import { AppRouter } from "./app-router";
 
 import "semantic-ui-css/semantic.min.css";
 import "./index.scss";
 
-ReactDOM.render(<AppRouter />, document.getElementById("root"));
+render(<AppRouter />, document.getElementById("root"));
